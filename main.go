84c3d1b65b578554d053e53ab7e997e1dbe7b6b7
orriginalo <originalo1717@gmail.com>
2025-04-29 20:59:26 +0400

    changed readme and added db configuration to main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -13,9 +14,14 @@ import (
 
 var db *gorm.DB
 
+const DB_HOST = "localhost" // "db" - если запущено в Docker-контейнере
+const DB_USERNAME = "postgres"
+const DB_PASSWORD = "postgres"
+const DB_NAME = "postgres"
+const DB_PORT = 5432
+
 func initDB() {
-	dsn := "host=db user=postgres password=postgres dbname=postgres port=5432"
-	// dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432"
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT)
 	var err error
 
 	db, err = gorm.Open(postgres.Open(dsn))
